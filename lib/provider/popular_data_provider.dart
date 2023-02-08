6a7c9518019bafab3580f0fc8ed43aaea9f3ad4c
Robin <sfurobin94@gmail.com>
2023-02-08 20:05:29 +0600

    messsss

--- lib/provider/popular_data_provider.dart
+++ lib/provider/popular_data_provider.dart
@@ -6,7 +6,7 @@ class PopularDataProvider with ChangeNotifier{
   PopularDataRepo? popularDataRepo = PopularDataRepo();
 
   List<Popular_BandsData>? _popularDataList=[];
-  List<Popular_BandsData>? get popularDataList => _popularDataList; //dfdfdfdf
+  List<Popular_BandsData>? get popularDataList => _popularDataList; //dfdfdfdfdfdfdfdfdsf
 
   getPopularData()async {
     _popularDataList= await popularDataRepo!.getPopularDataList();
